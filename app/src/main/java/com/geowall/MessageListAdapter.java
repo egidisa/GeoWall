813206dd47f25bcbb8213b6c7ea2e08c1e4d0105
egidisa <egidi.sara@gmail.com>
2016-05-18 18:54:02 +0200

    messagelist

--- app/src/main/java/com/geowall/MessageListAdapter.java
+++ app/src/main/java/com/geowall/MessageListAdapter.java
@@ -10,6 +10,8 @@ import com.geowall.FirebaseListAdapter;
 import com.geowall.R;
 import com.geowall.domain.Message;
 
+import org.w3c.dom.Text;
+
 
 /**
  * @author greg
@@ -22,6 +24,7 @@ public class MessageListAdapter extends FirebaseListAdapter<Message> {
 
     // The mUsername for this client. We use this to indicate which messages originated from this user
     private String mUsername;
+    private String timeStamp;
 
     public MessageListAdapter(Query ref, Activity activity, int layout, String mUsername) {
         super(ref, Message.class, layout, activity);
@@ -41,12 +44,15 @@ public class MessageListAdapter extends FirebaseListAdapter<Message> {
         // Map a Chat object to an entry in our listview
 
         String author = msg.getUid();
+        String time = msg.getTimestamp();
         TextView authorText = (TextView) view.findViewById(R.id.author);
-        authorText.setText(author + ": ");
+        TextView timeText = (TextView) view.findViewById(R.id.timetxt);
+        authorText.setText(author+" ");
+        timeText.setText(time);
         // If the message was sent by this user, color it differently
         if (author != null && author.equals(mUsername)) {
             authorText.setTextColor(Color.RED);
-
+            //TODO change this message layout, not just color
         } else {
             authorText.setTextColor(Color.BLUE);
 
